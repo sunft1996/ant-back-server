621a9e9df25032f1e704a96d85ea541eb9ffc2ac
sunfutao <810479416@qq.com>
2020-06-17 10:50:23 +0800

    优化代码

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 /*
  * @Descripttion: 
  * @Author: sunft
- * @Date: 2020-03-27 17:56:10
- * @LastEditTime: 2020-05-20 15:20:42
- */
+ * @Date: 2020-04-01 10:22:58
+ * @LastEditTime: 2020-06-17 10:47:06
+ */ 
+/* eslint-disable */
 
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
@@ -11,18 +12,21 @@ const session = require('koa-session');
 const sessionStore = require('./session');
 const static = require('koa-static');
 const path = require('path');
-const app = new Koa();
 const role = require('./routes/role');
 const user = require('./routes/user');
 const menu = require('./routes/menu');
 const article = require('./routes/article');
 const api = require('./routes/api');
 const demo = require('./routes/demo');
+const notLoginApi = ['/empty-item/login', '/empty-item/getCaptcha']
+const source = static(`${path.join(__dirname)}/public`);
+const app = new Koa();
+
+// session
 app.keys = ['sunft handsome!'];
-// session配置
+
 const CONFIG = {
     key: 'sessionId',
-    // session有效期一天
     maxAge: 86400000,
     autoCommit: true,
     overwrite: true,
@@ -30,33 +34,28 @@ const CONFIG = {
     signed: false,
     rolling: false,
     renew: false,
-    // session存储位置
     store: sessionStore
 };
-app.use(session(CONFIG, app));
 
-// POST
+app.use(session(CONFIG, app));
 app.use(bodyParser());
 
-const notLoginApi = ['/empty-item/login', '/empty-item/getCaptcha']
-
-/*
- * 请求拦截器
- */
-// eslint-disable-next-line max-statements
+// 请求拦截
 app.use(async (ctx, next) => {
-    // 未登录
+    
     if (notLoginApi.indexOf(ctx.path) === -1 && !ctx.session.isLogin && !(/\/uploadImg/).test(ctx.path)) {
         ctx.throw(401, '用户未登录');
         return;
     }
 
-    // 请求路径格式化
     let { path } = ctx;
+    
     path = path.replace(/(\w*)\/$/, '$1');
+    
     // 当前接口需要校验权限
     if (ctx.session.isLogin && ctx.session.allAuthApi.indexOf(path) !== -1) {
-        // 用户无权限
+        
+        // 无接口访问权限
         if (ctx.session.allowApi.indexOf(ctx.path) === -1) {
             ctx.status = 200;
             ctx.body = {
@@ -66,22 +65,26 @@ app.use(async (ctx, next) => {
             return;
         }
     }
-    // referer格式化 
+
     let { referer } = ctx.header;
+
     referer = referer.replace(/^\w+\:\/\/(\w|\.)+(\:\d+)?/, '');
     referer = referer.replace(/\?.*/, '')
     referer = referer.replace(/(\w*)\/$/, '$1');
-    // 用户无页面访问权限
+
+    // 无页面访问权限
     if (ctx.session.isLogin && ctx.session.allowPage.indexOf(referer) === -1 && path !== '/empty-item/sysUser/toUserDetails') {
         ctx.throw(403, '无权限');
         return;
     }
+
     await next();
 });
-// 静态资源统一放到public中管理
-const source = static(`${path.join(__dirname)}/public`);
+
+// 静态资源
 app.use(source);
-// routes
+
+// 路由
 app.use(role.routes(), role.allowedMethods());
 app.use(user.routes(), user.allowedMethods());
 app.use(menu.routes(), menu.allowedMethods());
